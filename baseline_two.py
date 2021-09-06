751c55112acf445442fda949a12086b3271884f2
simosss <35894696+simosss@users.noreply.github.com>
2021-09-06 20:03:28 +0300

    baseline-two use training function and concatenate left and right instead of adding (that made no sense)

--- baseline_two.py
+++ baseline_two.py
@@ -1,104 +1,19 @@
-from collections import defaultdict
 from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LogisticRegression
 from scipy import sparse
 from sklearn.decomposition import TruncatedSVD
 import pandas as pd
-from sklearn.metrics import f1_score, roc_auc_score, precision_recall_curve, auc
-from sklearn.naive_bayes import GaussianNB
-from sklearn.tree import DecisionTreeClassifier
+# from sklearn.naive_bayes import GaussianNB
+# from sklearn.tree import DecisionTreeClassifier
 from sklearn.experimental import enable_hist_gradient_boosting
-from sklearn.ensemble import RandomForestClassifier
+# from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import HistGradientBoostingClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-
-
-def load_combo_se(fname='data/bio-decagon-combo.csv'):
-    combo2stitch = {}
-    combo2se = defaultdict(set)
-    se2name = {}
-    drugs = set()
-    fin = open(fname)
-    print('Reading: %s' % fname)
-    fin.readline()
-    for line in fin:
-        stitch_id1, stitch_id2, se, se_name = line.strip().split(',')
-        if se not in se_500:
-            continue
-        combo = stitch_id1 + '_' + stitch_id2
-        combo2stitch[combo] = [stitch_id1, stitch_id2]
-        combo2se[combo].add(se)
-        se2name[se] = se_name
-        drugs.update([stitch_id1,stitch_id2])
-    fin.close()
-    n_interactions = sum([len(v) for v in combo2se.values()])
-    print('Drug combinations: %d Side effects: %d' % (len(combo2stitch), len(se2name)))
-    print('Drug-drug interactions: %d' % n_interactions)
-    return combo2stitch, combo2se, se2name, drugs
-
-
-def load_mono_se(fname='data/bio-decagon-mono.csv'):
-    stitch2se = defaultdict(set)
-    se2name = {}
-    fin = open(fname)
-    print('Reading: %s' % fname)
-    fin.readline()
-    for line in fin:
-        contents = line.strip().split(',')
-        stitch_id, se, = contents[:2]
-        se_name = ','.join(contents[2:])
-        stitch2se[stitch_id].add(se)
-        se2name[se] = se_name
-    return stitch2se, se2name
-
-
-def training(model, x_tr, x_te, yy, yy_test):
-    for i in range(yy.shape[1]):
-        print(i)
-        model.fit(x_tr, yy[:, i])
-        # print("finished training")
-        y_pred = model.predict(x_te)
-        y_prob = model.predict_proba(x_te)
-        # keep probability for the positive class only
-        y_prob = y_prob[:, 1]
-        f1_s = f1_score(yy_test[:, i], y_pred)
-        auroc_s = roc_auc_score(yy_test[:, i], y_prob)
-        precision, recall, thresholds = precision_recall_curve(yy_test[:, i], y_prob)
-        auprc_s = auc(recall, precision)
-        f1_scores.append(f1_s)
-        auroc_scores.append(auroc_s)
-        auprc_scores.append(auprc_s)
-    return f1_scores, auroc_scores, auprc_scores
-
-
-def training_with_split(model, X, Y):
-    for i in range(Y.shape[1]):
-        print(i)
-        # split into train and test
-        x_tr, x_te, y_tr, y_te = train_test_split(X, Y, test_size=0.2, random_state=42, stratify=Y[:, i])
-        model.fit(x_tr, y_tr[:, i])
-        print("finished training")
-        y_pred = model.predict(x_te)
-        y_prob = model.predict_proba(x_te)
-        # keep probability for the positive class only
-        y_prob = y_prob[:, 1]
-        f1_s = f1_score(y_te[:, i], y_pred)
-        auroc_s = roc_auc_score(y_te[:, i], y_prob)
-        precision, recall, thresholds = precision_recall_curve(y_te[:, i], y_prob)
-        auprc_s = auc(recall, precision)
-        f1_scores.append(f1_s)
-        auroc_scores.append(auroc_s)
-        auprc_scores.append(auprc_s)
-        frequency = y_te.sum(axis=0) / len(y_te)
-
-    return f1_scores, auroc_scores, auprc_scores, frequency
+from helpers import training_with_split, load_combo_se, load_mono_se
+from scipy.sparse import hstack
+from sklearn.naive_bayes import GaussianNB
 
 
 # load data ------------------------------------
-se_500 = pd.read_csv('se.csv')
-se_500 = se_500['# poly_side_effects'].to_list()
-
 combo2stitch, combo2se, se2name, drugs = load_combo_se()
 stitch2se, se2name_mono = load_mono_se()
 
@@ -137,6 +52,7 @@ del left, lef
 l.insert(0, [str(i) for i in range(len(mono_se_dict))])
 # remove first element from the list after the ohe is done correctly
 ll = mlb.fit_transform(l)
+# are you sure it is the last three you need to discard?
 ll = ll[1:, :10184]
 del l
 
@@ -149,7 +65,7 @@ rr = mlb.transform(r)
 rr = rr[1:, :10184]
 del r
 
-x = ll + rr
+x = hstack((ll, rr))
 del ll, rr
 
 # export dense dataset. Too big
@@ -161,7 +77,7 @@ x_sparse = sparse.csr_matrix(x)
 del x
 
 # PCA ------------------------------------------------
-svd = TruncatedSVD(n_components=300, random_state=42)
+svd = TruncatedSVD(n_components=100, random_state=42)
 lsa_x = svd.fit_transform(x_sparse)
 print(svd.explained_variance_ratio_.sum())
 
@@ -174,21 +90,16 @@ print(svd.explained_variance_ratio_.sum())
 # y_mini = y_train[:, :5]
 # y_mini_test = y_test[:, :5]
 
-#lr = LogisticRegression(random_state=1, max_iter=1000)
-#tree = DecisionTreeClassifier()
-boost = HistGradientBoostingClassifier()
+# lr = LogisticRegression(random_state=1, max_iter=1000)
+# tree = DecisionTreeClassifier()
+# boost = HistGradientBoostingClassifier()
+bayes = GaussianNB()
 
 # training -----------------------------------------------
-f1_scores = list()
-auroc_scores = list()
-auprc_scores = list()
-#f1, auroc, auprc = training(lr, x_train, x_test, y_train, y_test)
-f1, auroc, auprc, freq = training_with_split(boost, lsa_x, y[:, :10])
-# Frequency of the se
-#freq = y_test.sum(axis=0) / len(y_test)
+# f1, auroc, auprc = training(lr, x_train, x_test, y_train, y_test)
+f1, auroc, auprc, freq = training_with_split(bayes, lsa_x, y[:, :10])
+
 mean_auprc = sum(auprc) / len(auprc)
 mean_freq = sum(freq) / len(freq)
 df = pd.DataFrame({'auprc': auprc, 'auroc': auroc, 'f1_score': f1, 'freq': freq})
 df.to_csv('results/baseline_two/pca300_bayes.csv')
-
-
