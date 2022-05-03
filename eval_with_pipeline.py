b56e854bdfcbfa995a84f0ee08bc6eb19ac099ad
simos_panagiotoglou <simos.panagiotoglou@spitogatos.gr>
2022-05-03 09:52:56 +0300

    update eval with pipeline

--- eval_with_pipeline.py
+++ eval_with_pipeline.py
@@ -1,21 +1,30 @@
 from pykeen.triples import TriplesFactory
 from pykeen.evaluation import RankBasedEvaluator
 from pykeen.pipeline import pipeline
+import json
 
-n_tokeep = 200
+n_tokeep = 300
 minimum = 500
 
 tf = TriplesFactory.from_path(f'data/rare/rare_{minimum}_{n_tokeep}.csv')
-training, testing, validation = tf.split([.8, .1, .1])
+training, testing = tf.split([.8, .2])
 
 
 result_pipeline = pipeline(
     training=training,
     testing=testing,
     model='RESCAL',
-    epochs=100,
+    model_kwargs=dict(embedding_dim=300),
+    training_kwargs=dict(#sampler="schlichtkrull",
+                         # checkpoint_name='RGCN_checkpointt.pt',
+                         # checkpoint_frequency=5,
+                         num_epochs=200#,
+                         #batch_size=128
+),
     evaluator=RankBasedEvaluator,
     evaluator_kwargs=dict(ks=[50])
 )
+result_pipeline.plot_losses()
+
 
 result_pipeline.plot()
