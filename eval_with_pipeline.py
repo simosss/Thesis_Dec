5fe40391b54cad9db9bda93306fd6c36ccb9ebeb
simos_panagiotoglou <simos.panagiotoglou@spitogatos.gr>
2022-05-23 09:12:48 +0300

    eval with pipeline fo rescal train

--- eval_with_pipeline.py
+++ eval_with_pipeline.py
@@ -7,24 +7,53 @@ n_tokeep = 300
 minimum = 500
 
 tf = TriplesFactory.from_path(f'data/rare/rare_{minimum}_{n_tokeep}.csv')
-training, testing = tf.split([.8, .2])
+training, testing, validation = tf.split([.8, .1, .1])
 
+
+# read the feature vectors
+with open(f'results/feature_vectors_mono_mini.json', 'r') as f:
+    vectors = json.load(f)
 
 result_pipeline = pipeline(
     training=training,
     testing=testing,
-    model='RESCAL',
+    validation=validation,
+    model='rescal',
     model_kwargs=dict(embedding_dim=300),
     training_kwargs=dict(#sampler="schlichtkrull",
-                         # checkpoint_name='RGCN_checkpointt.pt',
-                         # checkpoint_frequency=5,
-                         num_epochs=200#,
+                         checkpoint_name='rescale_checkpoint_300_.pt',
+                         checkpoint_frequency=5,
+                         num_epochs=60#,
                          #batch_size=128
 ),
     evaluator=RankBasedEvaluator,
-    evaluator_kwargs=dict(ks=[50])
+    evaluator_kwargs=dict(ks=[10, 50]),
+    stopper='early',
+    stopper_kwargs=dict(
+        metric='hits_at_k',
+        frequency=4, patience=3, relative_delta=0.002)
 )
+
 result_pipeline.plot_losses()
 
 
-result_pipeline.plot()
+
+
+from pykeen.models import RGCN
+from torch.optim import Adam
+
+# Pick model, optimizer, training approach
+model = RGCN(triples_factory=training, embedding_dim=13, num_layers=2)
+optimizer = Adam(params=model.get_grad_params())
+training_loop = SLCWATrainingLoop(
+    model=model,
+    triples_factory=training,
+    optimizer=optimizer,
+)
+
+# train
+training_loop.train(
+    triples_factory=training,
+    num_epochs=500,
+    batch_size=256,
+)
