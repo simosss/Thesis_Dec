c4af1d928c2ba5cb90a0d67fce0c1c3d2042520b
simos_panagiotoglou <simos.panagiotoglou@spitogatos.gr>
2022-06-08 16:46:32 +0300

    working toy rgcn

--- rgcn_try.py
+++ rgcn_try.py
@@ -1,10 +1,13 @@
-from pykeen.models import ERModel, EntityRelationEmbeddingModel
-from pykeen.nn import EmbeddingSpecification, Embedding
+from pykeen.models import ERModel
+from pykeen.nn import Embedding
 import torch
 from pykeen.nn.modules import DistMultInteraction
 from pykeen.datasets import Nations
-import numpy as np
+from pykeen.training import SLCWATrainingLoop
+from torch.optim import Adam
 import pandas as pd
+
+
 # Pykeen class
 class PretrainedInitializer:
     """
@@ -44,37 +47,33 @@ class PretrainedInitializer:
     def __call__(self, x: torch.Tensor) -> torch.Tensor:
         """Initialize the tensor with the given tensor."""
         if x.shape != self.tensor.shape:
-            raise ValueError(f"shape does not match: expected {self.tensor.shape} but got {x.shape}")
+            raise ValueError(
+                f"shape does not match: expected {self.tensor.shape} but got {x.shape}")
         return self.tensor.to(device=x.device, dtype=x.dtype)
 
 
-class Pretrained_Emb(Embedding):
-
-    def __init__(self, ent_emb):
-        self.ent_emb = ent_emb
-        super().__init__(
-            num_embeddings=ent_emb.shape[0],
-            embedding_dim=ent_emb.shape[1],
-            trainable=False)
-
-    def forward(self, indices):
-        return self.ent_emb[indices]
-
-
 class DistMult(ERModel):
-    def _init_(
+    def __init__(
             self,
             ent_emb,
+            rel_emb,
             random_seed=42,
-            **kwargs,
+            **kwargs
     ) -> None:
         super().__init__(
             interaction=DistMultInteraction(),
             entity_representations=Embedding(
                 num_embeddings=ent_emb.shape[0],
-                shape=ent_emb.shape[1],
+                embedding_dim=3,
                 initializer=PretrainedInitializer(tensor=ent_emb),
-                trainable=False,
+                trainable=True,
+            ),
+
+            relation_representations=Embedding(
+                num_embeddings=rel_emb.shape[0],
+                embedding_dim=3,
+                initializer=PretrainedInitializer(tensor=rel_emb),
+                trainable=True,
             ),
 
             random_seed=random_seed,
@@ -84,51 +83,104 @@ class DistMult(ERModel):
 
 dataset = Nations()
 entity_embeddings = {0: [1, 2, 3],
-                     1: [4, 5, 6], 2: [1, 2, 3], 3: [4, 5, 6],
-                              4:[1, 2, 3], 5:[4, 5, 6], 6:[1, 2, 3], 7:[4, 5, 6],
-                              8:[1, 2, 3], 9:[4, 5, 6], 10:[1, 2, 3], 11:[4, 5, 6],
-                              12:[1, 2, 3], 13:[4, 5, 6]}
+                     1: [4, 5, 6],
+                     2: [1, 2, 3],
+                     3: [4, 5, 6],
+                     4:[1, 2, 3],
+                     5:[4, 5, 6],
+                     6:[1, 2, 3],
+                     7:[4, 5, 6],
+                     8:[1, 2, 3],
+                     9:[4, 5, 6],
+                     10:[1, 2, 3],
+                     11:[4, 5, 6],
+                     12:[1, 2, 3],
+                     13:[4, 5, 6]
+                     }
+relation_embedding = {0: [1, 2, 3],
+                     1: [4, 5, 6],
+                     2: [1, 2, 3],
+                     3: [4, 5, 6],
+                     4:[1, 2, 3],
+                     5:[4, 5, 6],
+                     6:[1, 2, 3],
+                     7:[4, 5, 6],
+                     8:[1, 2, 3],
+                     9:[4, 5, 6],
+                     10:[1, 2, 3],
+                     11:[4, 5, 6],
+                     12:[1, 2, 3],
+                     13:[4, 5, 6],
+                     14: [4, 5, 6],
+                     15: [1, 2, 3],
+                     16: [4, 5, 6],
+                              17:[1, 2, 3],
+                     18:[4, 5, 6],
+                     19:[1, 2, 3],
+                     20:[4, 5, 6],
+                     21:[1, 2, 3],
+                              22:[1, 2, 3],
+                     23:[4, 5, 6],
+                     24:[1, 2, 3],
+                     25:[4, 5, 6],
+                              26:[1, 2, 3],
+                     27:[4, 5, 6],
+                     28: [4, 5, 6],
+                     29: [1, 2, 3],
+                     30: [4, 5, 6],
+                              31:[1, 2, 3],
+                     32:[4, 5, 6],
+                     33:[1, 2, 3],
+                     34:[4, 5, 6],
+                              35:[1, 2, 3],
+                     36:[4, 5, 6],
+                     37:[1, 2, 3],
+                     38:[4, 5, 6],
+                              39:[1, 2, 3],
+                     40:[4, 5, 6],
+                     41: [4, 5, 6],
+                     42: [1, 2, 3],
+                     43: [4, 5, 6],
+                              44:[1, 2, 3],
+                     45:[4, 5, 6],
+                     46:[1, 2, 3],
+                     47:[4, 5, 6],
+                              48:[1, 2, 3],
+                     49:[4, 5, 6],
+                     50:[1, 2, 3],
+                     51:[4, 5, 6],
+                              52:[1, 2, 3],
+                     53:[4, 5, 6],
+                     54:[4, 5, 6]}
 
 
-# Creates a dataframe with keys as index and values as cell values.
 df = pd.DataFrame(entity_embeddings)
 array = df.values.T
-pe_ent = Pretrained_Emb(torch.Tensor(array))
+pe_ent = torch.Tensor(array)
 
-# till here everything is right
-distmult_model = DistMult(interaction=DistMultInteraction(),
-                          entity_representations=pe_ent,
+df2 = pd.DataFrame(relation_embedding)
+array2 = df2.values.T
+pe_rel = torch.Tensor(array2)
+
+
+distmult_model = DistMult(ent_emb=pe_ent,
+                          rel_emb=pe_rel,
                           triples_factory=dataset.training)
 
 
-
-
-
-
-# Get a training dataset
-from pykeen.datasets import Nations
-dataset = Nations()
-training_triples_factory = dataset.training
-
-# Pick a model
-from pykeen.models import TransE
-model = TransE(triples_factory=training_triples_factory)
-
 # Pick an optimizer from Torch
-from torch.optim import Adam
-optimizer = Adam(params=model.get_grad_params())
+optimizer = Adam(params=distmult_model.parameters())
 
 # Pick a training approach (sLCWA or LCWA)
-from pykeen.training import SLCWATrainingLoop
 training_loop = SLCWATrainingLoop(
-    model=model,
-    triples_factory=training_triples_factory,
+    model=distmult_model,
+    triples_factory=dataset.training,
     optimizer=optimizer,
 )
 
 # Train like Cristiano Ronaldo
 _ = training_loop.train(
-    triples_factory=training_triples_factory,
+    triples_factory=dataset.training,
     num_epochs=5,
     batch_size=256,
 )
