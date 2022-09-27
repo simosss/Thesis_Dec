4b5351efab619993f8cbe9bf2ee15bbfc1eb0920
simos_panagiotoglou <simos.panagiotoglou@spitogatos.gr>
2022-09-27 16:21:57 +0300

    fixed negative sampling script to include all drugs not create triplets with the same entity on tail and head, not discriminate among node1 and node2, not create negatives that are the inverse of a positive

--- negative_sampling.R
+++ negative_sampling.R
@@ -1,30 +1,60 @@
 library(tidyverse)
 
-combo <- read_csv('bio-decagon-combo.csv') %>% 
+# more than 500 appearances
+poly_se <- read_csv('se.csv')
+
+combo <- read_csv('data/bio-decagon-combo.csv') %>% 
     select(-"Side Effect Name") %>%
     rename(node1 = `STITCH 1`,
            node2 = `STITCH 2`,
-           se = `Polypharmacy Side Effect`)
+           se = `Polypharmacy Side Effect`) %>%
+    filter(se %in% poly_se$se)
+
+drugs <- unique(c(unique(combo$node1), unique(combo$node2)))
+
+inv_combo <- combo %>%
+    mutate(node_temp = node1,
+           node1 = node2,
+           node2 = node_temp) %>%
+    select(-node_temp)
+
+combo_1 <- combo %>%
+    slice(which(row_number() %% 2 == 1))
+
+combo_2 <- inv_combo %>%
+    slice(which(row_number() %% 2 == 0))
+
+data <- bind_rows(combo_1, combo_2)
+
+rm(combo_1, combo_2, inv_combo)
 
 
-drugs <- bind_rows(d1 =combo$node1, d2=combo$node2) %>%
-    mutate(node1 = if_else(is.na(d1), d2, d1)) %>%
-    distinct(node1) %>%
-    pull()
-
-
-data <- combo %>%
-    mutate(neg = sample(x=drugs, size= nrow(data), replace = TRUE)) %>%
-    unite(neg_pair, c("node1", "neg"), remove=FALSE) %>%
-    unite(pos_pair, c('node1', 'node2'), remove=FALSE) 
-
-pos_pairs <- data$pos_pair
-
-# many times untill check =0 for all
 data <- data %>%
-    mutate(check = if_else(neg_pair %in% pos_pairs, 1, 0)) %>%
-    mutate(neg = if_else(check == 1, sample(x=drugs, size= nrow(data), replace = TRUE), neg)) %>%
+    mutate(neg = sample(x=drugs, size = nrow(data), replace = TRUE)) %>%
+    unite(neg_pair, c("node1", "neg"), remove=FALSE) %>%
+    unite(pos_pair, c('node1', 'node2'), remove=FALSE) %>%
+    unite(inv_pos_pair, c('node2', 'node1'), remove=FALSE) 
+
+positive_pairs <- c(data$pos_pair, data$inv_pos_pair)
+
+data <- data %>%
+    mutate(check1 = if_else(neg_pair %in% positive_pairs, 1, 0),
+           check2 = if_else(node1 == neg, 1, 0)) %>%
+    mutate(neg = if_else(check1 == 1 | check2 == 1, sample(x=drugs, size= nrow(data), replace = TRUE), neg)) %>%
     unite(neg_pair, c("node1", "neg"), remove=FALSE)
+
+i <- 0
+# many times untill check =0 for all
+while(nrow(data %>% filter(check1 == 1 | check2 ==1)) != 0){
+    print(i)
+    data <- data %>%
+        mutate(check1 = if_else(neg_pair %in% positive_pairs, 1, 0),
+               check2 = if_else(node1 == neg, 1, 0)) %>%
+        mutate(neg = if_else(check1 == 1 | check2 == 1, sample(x=drugs, size= nrow(data), replace = TRUE), neg)) %>%
+        unite(neg_pair, c("node1", "neg"), remove=FALSE)
+    i <- i+1
+    
+}
 
 negative_samples <- data %>%
     select(node1, node2 = neg, se)
@@ -33,5 +63,5 @@ negative_samples <- data %>%
 positive_samples <- data %>%
     select(node1, node2, se)
 
-write_csv(positive_samples, 'positive_samples.csv')
-write_csv(negative_samples, 'negative_samples.csv')
+write_csv(positive_samples, 'positive_samples_uniform.csv')
+write_csv(negative_samples, 'negative_samples_uniform.csv')
